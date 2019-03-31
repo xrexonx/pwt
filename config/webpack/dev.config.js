b9de10f70c46b13d1f9895ac9c2071eb304fca14
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2019-03-31 12:42:52 +0800

    Add sample login template

--- config/webpack/dev.config.js
+++ config/webpack/dev.config.js
@@ -1,4 +1,4 @@
-const {resolve} = require('path');
+const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
